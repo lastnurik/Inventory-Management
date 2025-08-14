349ad811d0801de194bc2d4d9640c47aea01d360
lastnurik <es.nurik3@gmail.com>
2025-08-14 18:02:57 +0500

    oath2 with google

--- backend/InventoryManagement/InventoryManagement.Application/Interfaces/IAccountService.cs
+++ backend/InventoryManagement/InventoryManagement.Application/Interfaces/IAccountService.cs
@@ -1,11 +1,16 @@
 ﻿using InventoryManagement.Domain.Requests;
+using System.Security.Claims;
 
 namespace InventoryManagement.Application.Interfaces
 {
     public interface IAccountService
     {
         Task RegisterAsync(RegisterRequest registerRequest);
+
         Task LoginAsync(LoginRequest loginRequest);
+
         Task RefreshTokenAsync(string? refreshToken);
+
+        Task LoginWithGoogleAsync(ClaimsPrincipal? claimsPrincipal);
     }
 }
---
+++ backend/InventoryManagement/InventoryManagement.Domain/Exceptions/ExternalLoginProviderException.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Domain.Exceptions
+{
+    public class ExternalLoginProviderException(string provider, string message) : Exception($"External login provider: {provider} error occurred: {message}");
+}
